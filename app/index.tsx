2ad33b511bcc12f213a90dcda5d49b38b1abbb62
zhishui <zhi_shui@hunliji.com>
2016-07-09 14:58:07 +0800

    ts react

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,8 @@
-interface Props {
-	from: string;
-	message: string;
-}
-
-class Greeting extends React.Component<Props,any> {
-	render () {
-		const {greet} = this.props;
-		return (
-			<div>
-				<p>来自{greet.from}的消息</p>
-				<p>{greet.message}</p>
-			</div>
-		)
-	}
-}
+import * as React from "react";
+import {render} from "react-dom";
+import '../assets/css/base.css'
+import {App} from './container/App.tsx'
+render(
+    <App compiler="TypeScript" framework="React" />,
+    document.getElementById("root")
+)
